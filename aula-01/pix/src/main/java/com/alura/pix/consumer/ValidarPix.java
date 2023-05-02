3dfe16321454e680849bf7f316905bd60a56b89a
Eduardo Santana <ezambomsantana@gmail.com>
2023-05-01 21:41:12 -0300

    add key validation

--- aula-01/pix/src/main/java/com/alura/pix/consumer/ValidarPix.java
+++ aula-01/pix/src/main/java/com/alura/pix/consumer/ValidarPix.java
@@ -2,11 +2,23 @@ package com.alura.pix.consumer;
 
 import com.alura.pix.dto.PixDTO;
 import com.alura.pix.dto.PixStatus;
+import com.alura.pix.model.Key;
+import com.alura.pix.model.Pix;
+import com.alura.pix.repository.PixRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.alura.pix.repository.KeyRepository;
+
 @Service
 public class ValidarPix {
+
+    @Autowired
+    private KeyRepository keyRepository;
+
+    @Autowired
+    private PixRepository pixRepository;
 
     @KafkaListener(topics = "pix-topic", groupId = "group-1")
     public void process(PixDTO pixDTO) {
@@ -18,6 +30,13 @@ public class ValidarPix {
             pixDTO.setStatus(PixStatus.ERRO);
         }
 
+        Key origem = keyRepository.findByChave(pixDTO.getChaveOrigem());
+        Key destino = keyRepository.findByChave(pixDTO.getChaveDestino());
+
+        if (origem == null || destino == null) {
+            pixDTO.setStatus(PixStatus.ERRO);
+        }
+        pixRepository.save(Pix.toEntity(pixDTO));
     }
 
 }
